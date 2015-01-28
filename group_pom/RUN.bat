a94a0bbdb2a6e106ae706576d63de8b1f9cab668
he tony <hexs1984@gmail.com>
2015-01-28 23:35:58 +0800

    update
    update

--- group_pom/RUN.bat
+++ group_pom/RUN.bat
@@ -1,1 +1,3 @@
-cmd
+call mvn install:install-file -Dfile=study-parent.xml -DgroupId=com.study -DartifactId=study-parent -Dversion=1.5 -Dpackaging=pom 
+call mvn install:install-file -Dfile=study-logs.xml -DgroupId=com.study -DartifactId=study-logs -Dversion=1.5.8 -Dpackaging=pom 
+call mvn install:install-file -Dfile=study-common.xml -DgroupId=com.study -DartifactId=study-common -Dversion=1.5.8 -Dpackaging=pom 
